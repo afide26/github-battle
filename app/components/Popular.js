ea3364b65d5d9d42420fc7cf627d0d1f628c71d5
Alan Fidelino <afidedev26@gmail.com>
2018-02-08 21:35:58 +1100

    Create SelectLanguage component

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,27 @@
 var React = require('react');
+var PropTypes = require('prop-types');
 
+function SelectLanguage(props){
+  var languages=['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python' ];
 
+  return(
+    languages.map(function(lang){
+      return (
+        <li 
+          style = {lang === props.language ? {color:'#d0021b', borderBottom:'1px solid #d0021b'}: null}
+          key={lang} 
+          onClick={props.onSelect.bind(null, lang)}>
+          {lang}
+        </li>
+      )
+    })
+  )
+}
+
+SelectLanguage.propTypes ={
+  language: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired
+}
 class Popular extends React.Component {
   constructor(props){
     super(props);
@@ -21,19 +42,13 @@ class Popular extends React.Component {
   }
 
   render(){
-    var languages=['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python' ];
+
     return(
       <ul className="languages">
-        {languages.map(function(lang){
-          return(
-            <li 
-              style = {lang === this.state.selectedLanguage ? {color:'#d0021b', borderBottom:'1px solid #d0021b'}: null}
-              key={lang} 
-              onClick={this.updateLanguage.bind(null, lang)}>
-              {lang}
-            </li>
-          )
-        }, this)}
+        <SelectLanguage 
+          language={this.state.selectedLanguage}
+          onSelect={this.updateLanguage}
+        />
       </ul>
     )
   }
