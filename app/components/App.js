dae657d866a2d46e31bdcee7bd4b1cda034754d4
Alan Fidelino <afidedev26@gmail.com>
2018-02-10 21:04:15 +1100

    Finish router components

--- app/components/App.js
+++ app/components/App.js
@@ -2,11 +2,13 @@ var React = require('react');
 var Home = require('./Home');
 var Battle = require('./Battle');
 var Nav = require('./Nav');
+var NotFound = require('./NotFound');
 var ReactRouter = require('react-router-dom');
 
 var Router = ReactRouter.BrowserRouter;
 var Route = ReactRouter.Route;
 var Link = ReactRouter.Link;
+var Switch = ReactRouter.Switch;
 
 var Popular = require('./Popular');
 
@@ -16,9 +18,12 @@ class App extends React.Component {
       <Router>
         <div className="container">
           <Nav/>
-          <Route path="/" exact component={Home}/>
-          <Route path="/battle" component={Battle}/>
-          <Route path="/popular" component={Popular}/>
+          <Switch>
+            <Route path="/" exact component={Home}/>
+            <Route path="/battle" exact component={Battle}/>
+            <Route path="/popular" component={Popular}/>
+            <Route component={NotFound}/>
+          </Switch>
         </div>
       </Router>
     )
---
+++ app/components/NotFound.js
@@ -0,0 +1,12 @@
+var React = require('react');
+
+
+function NotFound() {
+    return(
+      <div className="notfound">
+        <h1 className="notfound-title">404</h1>
+        <h2 className="notfound-subtitle">The page you are after does not exist in this application</h2>
+      </div>
+    )
+}
+module.exports = NotFound;
